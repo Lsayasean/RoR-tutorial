107c110b26dca81cb38d38199c6450ea4a13a197
Lanrna Sayasean <lsayasean@gmail.com>
2019-03-04 17:41:04 -0500

    following a youtube tutorial

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'posts', to:'posts#index'
+  get 'post/new', to:'posts#new'
+
+  post 'posts', to: 'posts#create'
+end
